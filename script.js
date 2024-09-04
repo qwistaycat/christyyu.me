27f01a5e3e66b8543fa75b2894e57f9aab2e979e
qwistaycat <christyyu2003@gmail.com>
2024-09-04 22:17:01 +0900

    buttons are now funny

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+const menu = document.querySelector('.menu');
+
+const menuIcon = document.querySelector('.menu_dark');
+
+menuIcon.addEventListener('mouseover', () => menuIcon.style.width = '40px');
+menuIcon.addEventListener('mouseout', () => menuIcon.style.width = '30px');
+
+// menuIcon.addEventListener('click', () => {
+//
+// });
+
+
+
+const moonIcon = document.querySelector('.moon');
+moonIcon.addEventListener('mouseover', () => moonIcon.style.width = '40px');
+moonIcon.addEventListener('mouseout', () => moonIcon.style.width = '30px');
