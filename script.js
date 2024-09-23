552acbafdbbf9363c0165055e9120d34c36b70f2
qwistaycat <christyyu2003@gmail.com>
2024-09-23 16:07:18 -0700

    hai

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
-// const menu = document.querySelector('.menu');
+const menu = document.querySelector('.menu');
 
-// const menuIcon = document.querySelector('.menu_dark');
-
-// menuIcon.addEventListener('click', () => {
-//     menu.style.backgroundColor = "black";
-
-// });
+const menuIcon = document.querySelector('.menu_dark');
+let menuOpen = false;
+menuIcon.addEventListener('click', () => {
+    if (menuOpen) {
+        menu.style.opacity = "0%";
+        menuOpen = false;
+    }
+    else{
+        menu.style.opacity = "85%";
+        menuOpen = true;
+    }
+});
